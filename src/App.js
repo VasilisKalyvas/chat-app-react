e063cfd2d12db42227c66c5b536508ccc88713c5
Βασιλης Καλυβας <bkalivas8@gmail.com>
2023-10-12 16:04:33 +0300

    socket

--- src/App.js
+++ src/App.js
@@ -11,16 +11,36 @@ function App() {
   const [message, setMessage] = useState('')
   const [onlineUsers, setOnlineUsers] = useState([])
   const messagesContainerRef = useRef(null);
+  const [isTyping, setIsTyping] = useState(false);
+  const [userTyping, setUserTyping] = useState('')
 
   const handleSendMessage = () => {
     if(!message?.length) return
     socket.emit('send-message', {message, user: username})
     setMessage('')
   } 
+  const typingTimeout = 2000;
+  let typingTimer = null;
+  
+  const handleTyping = () => {
+    clearTimeout(typingTimer);
+  
+    if (message?.length > 0 && !isTyping) {
+      socket.emit('typing', { isTyping: true, username });
+      setIsTyping(true);
+    }
+  
+    typingTimer = setTimeout(() => {
+      if (isTyping) {
+        socket.emit('typing', { isTyping: false, username });
+        setIsTyping(false);
+      }
+    }, typingTimeout);
+  };
 
   const scrollDown = () => {
     if( messagesContainerRef?.current){
-      messagesContainerRef?.current?.scrollIntoView();
+      messagesContainerRef?.current?.scrollIntoView({behavior: 'smooth'});
     }
   };
 
@@ -30,11 +50,23 @@ function App() {
     }
   };
 
+  const handleInputKeyPressSubmit = (e) => {
+    if (e.key === 'Enter') {
+      setUsername(name)
+    }
+  };
+
+  const handleLogout = () => {
+    socket.emit('logout', username)
+    setUsername('')
+  }
+
   useEffect(() => {
     if(username){
       socket.emit('username', username)
+      messagesContainerRef?.current?.scrollIntoView({behavior: 'smooth'});
     }
-  }, [socket, username])
+  }, [ username])
 
   useEffect(() =>{
     socket.on('online', (onlineUsers) => {
@@ -44,23 +76,44 @@ function App() {
     socket.on('messages', (messages) => {
       setMessages(messages)
     })
-  }, [socket])
+
+    socket.on('typing', ({isTyping, username}) => {
+      setIsTyping(isTyping)
+      setUserTyping(username)
+    })
+
+  }, [])
 
   useEffect(() => {
     scrollDown();
-  }, [messages])
+  }, [messages, isTyping])
   
+  useEffect(() => {
+    socket.on('heartbeat', () => {
+    });
+  
+    // Set a timeout to detect disconnections
+    const disconnectTimeout = setTimeout(() => {
+      console.log('Disconnected from the server');
+      // Handle the disconnection (e.g., update UI)
+    }, 15000); // Disconnect if no heartbeat received for 15 seconds
+  
+    return () => {
+      clearTimeout(disconnectTimeout);
+    };
+  }, []);
+
   return (
     <div className="App">
 
       {
         username?.length 
         ?
-         <h3>Hi {username}</h3> 
+         <div>Welcome <span>{username}</span><span className='logout' onClick={handleLogout}>Logout</span></div> 
         :
           <>
             <span>Username: </span>
-            <input onChange={(e) => setName(e.target.value)}/>
+            <input onKeyPress={handleInputKeyPressSubmit} onChange={(e) => setName(e.target.value)}/>
             <button onClick={()=> setUsername(name)}>Sumbit</button>
           </>
       }
@@ -69,7 +122,7 @@ function App() {
         onlineUsers?.length
         ?
           <>
-           <div style={{marginTop: '1rem'}} >Online Users:</div>
+           <div style={{marginTop: '5px'}} >Online Users: {onlineUsers?.length}</div>
            <div className='online'>
             {   
               onlineUsers?.map((user, index) => 
@@ -91,25 +144,40 @@ function App() {
         username
         ?
           <>
-            <div style={{marginTop: '1rem'}}>
-                <input value={message}  onKeyPress={handleInputKeyPress} onChange={(e) => setMessage(e.target.value)}/>
-                <button onClick={handleSendMessage}>Send</button>
-            </div>
-            <div style={{marginTop: '3rem'}}>
+            <div style={{marginTop: '0.4rem'}}>
                 Messages:
-                <div className='message-container'>
-                  {
-                    messages?.map((item, index) => (
-                      <ul key={index}>
-                        <li>
-                          {socket?.id === item.socketId ? 'You': item.user}: {item.message}
-                        </li>
-                      </ul>
-                    ))
-                  }
-                  <div ref={messagesContainerRef}/>
+                <div>
+                  <div className='message-container'>
+                    {
+                      messages?.map((item, index) => (
+                        <ul key={index}>
+                          {
+                            item.socketId === socket.id && item?.isAdmin
+                            ? null
+                            :
+                            <li className={`${item?.user === 'Admin' ? 'admin-message' : ''}`}>
+                              {item.user === username ? 'You': item.user}: {item.message}
+                            </li>
+                          }
+                        </ul>
+                      ))
+                    }
+                    <div ref={messagesContainerRef}/>
+                  </div>
+                 <div className='typing-container'>
+                    {
+                      userTyping?.length && isTyping
+                      ?
+                        <div>{`${userTyping} is typing...`}</div>
+                      : null
+                    }
+                 </div>
+                 
                 </div>
-                
+            </div>
+            <div style={{marginTop: '0.5rem'}}>
+                <input value={message}  onKeyPress={handleInputKeyPress} onChange={(e) => { setMessage(e.target.value); handleTyping();}}/>
+                <button onClick={handleSendMessage}>Send</button>
             </div>
           </>
         :
